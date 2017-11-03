b593defaa04188d74f4de942137193ff2edcaac3
codermaz <yilmazadem@gmail.com>
2017-11-04 00:27:44 +0100

    Initial Commit

---
+++ Table Views 50/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table Views 50
+//
+//  Created by MacMini on 04.11.17.
+//  Copyright © 2017 MacMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
